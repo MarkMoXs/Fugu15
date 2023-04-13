5fa9f4938a61432919e7fde2ff8d54ab2af56e48
Liam Huang <liamhuang0205@gmail.com>
2023-04-13 18:50:35 +0800

    [Packages] add reboot_userspace after jailbreak.

--- Fugu15/Fugu15/JailbreakView.swift
+++ Fugu15/Fugu15/JailbreakView.swift
@@ -14,40 +14,40 @@ enum JBStatus {
     case inProgress
     case failed
     case done
-    
+
     func text() -> String {
         switch self {
         case .notStarted:
             return "Jailbreak"
-            
+
         case .unsupported:
             return "Unsupported"
-            
+
         case .inProgress:
             return "Jailbreaking..."
-            
+
         case .failed:
             return "Error!"
-            
+
         case .done:
-            return "Jailbroken"
+            return "Jailbroken!"
         }
     }
-    
+
     func color() -> Color {
         switch self {
         case .notStarted:
             return .accentColor
-            
+
         case .unsupported:
             return .accentColor
-            
+
         case .inProgress:
             return .accentColor
-            
+
         case .failed:
             return .red
-            
+
         case .done:
             return .green
         }
@@ -56,18 +56,18 @@ enum JBStatus {
 
 struct JailbreakView: View {
     @Binding var logText: String
-    
+
     @State var status: JBStatus = .notStarted
     @State var textStatus1      = "Status: Not running"
     @State var textStatus2      = ""
     @State var textStatus3      = ""
     @State var showSuccessMsg   = false
-    
+
     var body: some View {
         VStack {
             Button(status.text(), action: {
                 status = .inProgress
-                
+
                 DispatchQueue(label: "Fugu15").async {
                     launchExploit()
                 }
@@ -77,7 +77,7 @@ struct JailbreakView: View {
                 .cornerRadius(10)
                 .foregroundColor(Color.white)
                 .disabled(status != .notStarted)
-            
+
             Text(textStatus1)
                 .padding([.top, .leading, .trailing])
                 .font(.headline)
@@ -90,40 +90,51 @@ struct JailbreakView: View {
                 .font(.footnote)
                 .opacity(0.4)
         }.alert(isPresented: $showSuccessMsg) {
-            Alert(title: Text("Success"), message: Text("Post environment started successfully, system wide injection will only affect newly spawned processes for now!"), dismissButton: .default(Text("OK")))
+            Alert(
+                title: Text("Success"),
+                message: Text("Post environment started successfully, " +
+                              "system wide injection will only affect newly spawned processes for now.\n" +
+                              "Click OK to reboot userspace!"),
+                dismissButton: .default(
+                    Text("OK"),
+                    action: {
+                        execCmd(args: ["/var/jb/usr/bin/launchctl","reboot","userspace"])
+                    }
+                )
+            )
         }
     }
-    
+
     func print(_ text: String, ender: String = "\n") {
         NSLog(text)
         logText += text + ender
     }
-    
+
     func statusUpdate(_ s: String) {
         textStatus3 = textStatus2
         textStatus2 = textStatus1
         textStatus1 = s
     }
-    
+
     func launchExploit() {
         do {
             statusUpdate("Status: Launching kexploitd")
-            
+
             try Fugu15.launchKernelExploit(oobPCI: Bundle.main.bundleURL.appendingPathComponent("oobPCI")) { msg in
                 if status != .done {
                     DispatchQueue.main.async {
                         if msg.hasPrefix("Status: ") {
                             statusUpdate(msg)
                         }
-                        
+
                         print(msg)
                     }
                 }
             }
-            
+
             try Fugu15.startEnvironment()
             //try Fugu15.launch_iDownload()
-            
+
             DispatchQueue.main.async {
                 statusUpdate("Status: Done!")
                 status = .done
@@ -140,7 +151,7 @@ struct JailbreakView: View {
 
 struct JailbreakView_Previews: PreviewProvider {
     @State static var logText = ""
-    
+
     static var previews: some View {
         JailbreakView(logText: $logText)
     }
